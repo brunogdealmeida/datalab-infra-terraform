d38ab95882b5954b377ee0a4e262112c5d6518a9
Bruno Gomes <brunogdealmeida@gmail.com>
2026-05-07 10:47:54 -0300

    Fixing duplicated declaration

--- modules/secret_manager/main.tf
+++ modules/secret_manager/main.tf
@@ -7,15 +7,6 @@ resource "google_secret_manager_secret" "secret" {
   }
 }
 
-resource "google_secret_manager_secret" "secret" {
-  project   = var.project_id
-  secret_id = var.secret_id
-
-  replication {
-    auto {}
-  }
-}
-
 resource "google_secret_manager_secret_iam_member" "accessors" {
   for_each = toset(var.accessors)
 
@@ -25,11 +16,3 @@ resource "google_secret_manager_secret_iam_member" "accessors" {
   member    = "serviceAccount:${each.value}"
 }
 
-resource "google_secret_manager_secret_iam_member" "accessor" {
-  for_each = toset(var.accessors)
-
-  project   = var.project_id
-  secret_id = google_secret_manager_secret.secret.secret_id
-  role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:${each.value}"
-}
