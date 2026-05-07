6805dde4e6ba71e66c8555746d73017fddf95fde
Bruno Gomes <brunogdealmeida@gmail.com>
2026-05-07 11:23:02 -0300

    fix: remove secret version output

--- modules/secret_manager/outputs.tf
+++ modules/secret_manager/outputs.tf
@@ -7,8 +7,3 @@ output "secret_name" {
   description = "Full secret resource name."
   value       = google_secret_manager_secret.secret.name
 }
-
-output "secret_version" {
-  description = "Latest managed secret version resource name (null if secret_data is empty)."
-  value       = one(google_secret_manager_secret_version.version[*].name)
-}
